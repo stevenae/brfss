86424c57d6e0e2dd31d7b81cb48dfb7338d2bf05
Steven Ellis <steven@divvyhomes.com>
2020-07-14 14:40:56 -0700

    examinig oregon by year

--- brfss_smart_scratch.R
+++ brfss_smart_scratch.R
@@ -4,11 +4,11 @@ by_cnty <- lapply(Sys.glob('~/Documents/brfss_smart_data/*'),read_xpt)
 name_overlap <- Reduce(intersect, lapply(by_cnty[-1],names))
 # sort(name_overlap)
 
-by_cnty <- lapply(by_cnty,function(x){x[,name_overlap]})
+by_cnty <- lapply(by_cnty[-1],function(x){x[,name_overlap]})
 by_cnty <- do.call(rbind,by_cnty)
-
-cnty02$A_STATE <- factor(cnty02$A_STATE)
-levels(cnty02$A_STATE) <- c("ALABAMA", "ALASKA", "ARIZONA", "ARKANSAS", "CALIFORNIA", 
+names(by_cnty) <- make.names(names(by_cnty))
+by_cnty$X_STATE <- factor(by_cnty$X_STATE)
+levels(by_cnty$X_STATE) <- c("ALABAMA", "ALASKA", "ARIZONA", "ARKANSAS", "CALIFORNIA", 
 	"COLORADO", "CONNECTICUT", "DELAWARE", "DISTRICT OF COLUMBIA", 
 	"FLORIDA", "GEORGIA", "HAWAII", "IDAHO", "ILLINOIS", "INDIANA",
 	"IOWA", "KANSAS", "KENTUCKY", "LOUISIANA", "MAINE", "MARYLAND",
@@ -21,9 +21,9 @@ levels(cnty02$A_STATE) <- c("ALABAMA", "ALASKA", "ARIZONA", "ARKANSAS", "CALIFOR
 	"WEST VIRGINIA", "WISCONSIN", "WYOMING", "GUAM", "PUERTO RICO",
 	"U.S. VIRGIN ISLANDS")
 
-with(subset(cnty02,A_STATE=='OREGON'),table(A_CNTYNA))
+with(subset(by_cnty,X_STATE=='OREGON'),table(X_CNTYNAM,IYEAR))
 
-
+table(by_cnty$IYEAR)
 
 
 
