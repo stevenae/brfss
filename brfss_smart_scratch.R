40869e1ece5682fbf98947fed63654251c16546c
Steven Ellis <steven@divvyhomes.com>
2020-07-14 14:34:15 -0700

    Update brfss_smart_scratch.R

--- brfss_smart_scratch.R
+++ brfss_smart_scratch.R
@@ -1,12 +1,31 @@
 library('haven')
-mmsa02 <- read_xpt('~/Downloads/MMSA02.xpt')
-t(mmsa02[1,])
-table(mmsa02$A_MMSANA)
-table(mmsa02$A_MMSA)
+by_cnty <- lapply(Sys.glob('~/Documents/brfss_smart_data/*'),read_xpt)
 
-chi <- subset(mmsa02,grepl('Chicago',A_MMSANA))
-t(summary(chi))
-chi$INCOME2
+name_overlap <- Reduce(intersect, lapply(by_cnty[-1],names))
+sort(name_overlap)
 
-cnty02 <- read_xpt('~/Downloads/CNTY02.XPT')
-t(cnty02[1,])
+by_cnty <- do.call(rbind,by_cnty)
+
+cnty02$A_STATE <- factor(cnty02$A_STATE)
+levels(cnty02$A_STATE) <- c("ALABAMA", "ALASKA", "ARIZONA", "ARKANSAS", "CALIFORNIA", 
+	"COLORADO", "CONNECTICUT", "DELAWARE", "DISTRICT OF COLUMBIA", 
+	"FLORIDA", "GEORGIA", "HAWAII", "IDAHO", "ILLINOIS", "INDIANA",
+	"IOWA", "KANSAS", "KENTUCKY", "LOUISIANA", "MAINE", "MARYLAND",
+	"MASSACHUSETTS", "MICHIGAN", "MINNESOTA", "MISSISSIPPI", 
+	"MISSOURI", "MONTANA", "NEBRASKA", "NEVADA", "NEW HAMPSHIRE",
+	"NEW JERSEY", "NEW MEXICO", "NEW YORK", "NORTH CAROLINA", 
+	"NORTH DAKOTA", "OHIO", "OKLAHOMA", "OREGON", "PENNSYLVANIA",
+	"RHODE ISLAND", "SOUTH CAROLINA", "SOUTH DAKOTA", "TENNESSEE",
+	"TEXAS", "UTAH", "VERMONT", "VIRGINIA", "WASHINGTON",
+	"WEST VIRGINIA", "WISCONSIN", "WYOMING", "GUAM", "PUERTO RICO",
+	"U.S. VIRGIN ISLANDS")
+
+with(subset(cnty02,A_STATE=='OREGON'),table(A_CNTYNA))
+
+
+
+
+
+
+
+
