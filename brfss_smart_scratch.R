07ba31cf3fb19da950ea43ddc30b042dc0fbceb2
Steven Ellis <steven@divvyhomes.com>
2020-07-21 14:21:15 -0700

    cleaner exclude code

--- brfss_smart_scratch.R
+++ brfss_smart_scratch.R
@@ -100,29 +100,30 @@ model_iters <- function(brfss) {
 																								 times = 1)
 		va_rows <- setdiff(seq(nrow(dat_trva)),tr_rows)
 		dat_oos <- subset(brfss,IYEAR == oos_yr)
-		
+		lapply(brfss,class)
 		# 7/21/20 We'll cut the below, all too closely related
 		# 1:  USEEQUIP 2.728468e-01 9.605390e-02 0.039149653
 		# 2:  POORHLTH 1.324804e-01 4.591618e-02 0.041885919
 		# 3:   GENHLTH 1.262460e-01 8.154534e-02 0.035571459
 		# 4:  PHYSHLTH 1.012915e-01 4.656002e-02 0.023784466
-		# 5:    EMPLOY 5.751206e-02 6.283269e-02 0.046937487
 		# 6:  X_RFHLTH 5.433353e-02 1.446244e-02 0.005683014
 		# 9:  MENTHLTH 1.228678e-02 1.997885e-02 0.023363502
-		# 
-		cut_vars <- c('USEEQUIP','POORHLTH','GENHLTH','PHYSHLTH','EMPLOY','X_RFHLTH','MENTHLTH')
+		# INTVID is consistently in the top 10...very curious, but would confound the planned analysis\
+		# Same with SEQNO
+		cut_vars <- c('USEEQUIP','POORHLTH','GENHLTH','PHYSHLTH','X_RFHLTH','MENTHLTH',
+									'outcome','QLACTLM2','X_CNTYWT','X_WT2','ADJCNTY','INTVID')
 		
-		tr_packaged <- xgb.DMatrix(data.matrix(dat_trva[tr_rows,!c('outcome','QLACTLM2')]),
+		tr_packaged <- xgb.DMatrix(data.matrix(dat_trva[tr_rows,!..cut_vars]),
 															 label=dat_trva[tr_rows,outcome]
-															 # ,weight=dat_trva[tr_rows,X_CNTYWT]
+															 ,weight=dat_trva[tr_rows,X_CNTYWT]
 		)
-		va_packaged <- xgb.DMatrix(data.matrix(dat_trva[va_rows,!c('outcome','QLACTLM2')]),
+		va_packaged <- xgb.DMatrix(data.matrix(dat_trva[va_rows,!..cut_vars]),
 															 label=dat_trva[va_rows,outcome]
-															 # ,weight=dat_trva[va_rows,X_CNTYWT]
+															 ,weight=dat_trva[va_rows,X_CNTYWT]
 		)
-		oos_packaged <- xgb.DMatrix(data.matrix(dat_oos[,!c('outcome','QLACTLM2',)]),
+		oos_packaged <- xgb.DMatrix(data.matrix(dat_oos[,!..cut_vars]),
 																label=dat_oos[,outcome]
-																# ,weight=dat_oos[,X_CNTYWT]
+																,weight=dat_oos[,X_CNTYWT]
 		)
 		
 		# run xgb iters
@@ -134,7 +135,7 @@ model_iters <- function(brfss) {
 				tree_method = 'hist',
 				grow_policy = 'lossguide',
 				early_stopping_rounds = 20,
-				# verbose=F,
+				verbose=F,
 				maximize=T,
 				# feval=evalerror,
 				eval_metric = 'auc',
@@ -146,7 +147,7 @@ model_iters <- function(brfss) {
 		# 	return(tr_va_xgb_m)
 		# })
 		
-		xgb.importance(model=tr_va_xgb_m)
+		head(xgb.importance(model=tr_va_xgb_m),20)
 		
 		
 		
